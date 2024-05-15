1a9838bc48a9fc0ce8cf9e2a0342c7565d4ec413
Adrián Bernárdez Cornes <adrian.bernardez@factorial.co>
2024-05-15 13:53:30 +0200

    emd

--- src/test/resources/example.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    CASE WHEN payload.op LIKE 'c' OR payload.op LIKE 'u' OR payload.op LIKE 'r' THEN payload.after
-    CASE WHEN payload.op LIKE 'd' THEN payload.before
-           END                      as record,
-    payload.op                     as op,
-    metadata_updated_at            as metadata_updated_at
-from bronze_table
