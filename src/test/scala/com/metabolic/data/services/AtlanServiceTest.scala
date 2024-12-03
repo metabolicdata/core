cfb6740408a89ec26de3712a08308aac6a1a5e79
fazpiazu <fernando.azpiazu@factorial.co>
2024-12-03 11:04:42 +0100

    Updated Atlan test

--- src/test/scala/com/metabolic/data/services/AtlanServiceTest.scala
+++ src/test/scala/com/metabolic/data/services/AtlanServiceTest.scala
@@ -2,17 +2,25 @@ package com.metabolic.data.services
 
 import com.metabolic.data.RegionedTest
 import com.metabolic.data.core.services.util.ConfigReaderService
+import com.metabolic.data.mapper.services.ConfigParserService
 import org.scalatest.funsuite.AnyFunSuite
 
 class AtlanServiceTest extends AnyFunSuite with RegionedTest  {
 
   test("atlanCatalogServiceTest") {
 
-    val configService = new ConfigReaderService()
+    val rawConfig = new ConfigReaderService().getConfig("src/test/resources/employees.conf")
 
-    // Load config file from path
-    val config = configService.getConfig("src/test/resources/employees.conf")
+    val config = new ConfigParserService()
+      .parseConfig(rawConfig)
 
-    asset(config != null)
+    val owner = config.head.owner
+    val sqlFile = config.head.sqlUrl
+    val confFile = config.head.confUrl
+
+    assert(owner != "")
+    assert(sqlFile != "")
+    assert(confFile != "")
+  }
 }
 
