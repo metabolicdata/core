edaa4111fb38cd7b42c2be665f25cde1f0d09719
Brais López Chao <brais.lopez@factorial.co>
2025-05-27 09:41:20 +0200

    refactor writer class

--- src/main/scala/com/metabolic/data/core/services/spark/writer/stream/KafkaWriter.scala
+++ src/main/scala/com/metabolic/data/core/services/spark/writer/stream/KafkaWriter.scala
@@ -3,71 +3,61 @@ package com.metabolic.data.core.services.spark.writer.stream
 import com.metabolic.data.core.services.spark.writer.DataframeUnifiedWriter
 import com.metabolic.data.mapper.domain.io.WriteMode
 import com.metabolic.data.mapper.domain.io.WriteMode.WriteMode
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.streaming.StreamingQuery
-import org.apache.spark.sql.{DataFrame, SaveMode}
 
-class KafkaWriter(servers: Seq[String], apiKey: String, apiSecret: String, topic: String,
-                  idColumnName: Option[String] = None, val checkpointLocation: String)
-  extends DataframeUnifiedWriter {
-  
+class KafkaWriter(
+                   servers: Seq[String],
+                   apiKey: String,
+                   apiSecret: String,
+                   topic: String,
+                   idColumnName: Option[String] = None,
+                   val checkpointLocation: String
+                 ) extends DataframeUnifiedWriter {
+
   override val output_identifier: String = topic
-
   override val writeMode: WriteMode = WriteMode.Append
 
-  private def setWriteAuthentication(writer: org.apache.spark.sql.DataFrameWriter[_]): org.apache.spark.sql.DataFrameWriter[_] = {
-    if (apiKey.isEmpty || apiSecret.isEmpty) {
-      writer
-    } else {
-      writer
-        .option("kafka.security.protocol", "SASL_SSL")
-        .option("kafka.sasl.mechanism", "PLAIN")
-        .option("kafka.sasl.jaas.config",
-          s"org.apache.kafka.common.security.plain.PlainLoginModule required username='$apiKey' password='$apiSecret';"
-        )
+  private val commonKafkaOptions: Map[String, String] = Map(
+    "kafka.bootstrap.servers" -> servers.mkString(","),
+    "topic" -> topic,
+    "kafka.session.timeout.ms" -> "45000",
+    "kafka.client.dns.lookup" -> "use_all_dns_ips"
+  )
+
+  private val authOptions: Map[String, String] =
+    if (apiKey.nonEmpty && apiSecret.nonEmpty)
+      Map(
+        "kafka.security.protocol" -> "SASL_SSL",
+        "kafka.sasl.mechanism" -> "PLAIN",
+        "kafka.sasl.jaas.config" ->
+          s"""org.apache.kafka.common.security.plain.PlainLoginModule required username="$apiKey" password="$apiSecret";"""
+      )
+    else Map.empty
+
+  private def prepareKafkaDf(df: DataFrame): DataFrame =
+    idColumnName match {
+      case Some(idCol) => df.selectExpr(s"$idCol as key", "to_json(struct(*)) as value")
+      case None        => df.selectExpr("to_json(struct(*)) as value")
     }
-  }
 
   override def writeStream(df: DataFrame): StreamingQuery = {
+    val kafkaDf = prepareKafkaDf(df)
 
-    val kafkaDf = idColumnName match {
-      case Some(c) => df.selectExpr(s"$c as key", "to_json(struct(*)) as value")
-      case None => df.selectExpr("to_json(struct(*)) as value")
-    }
-
-    kafkaDf
-      .writeStream
+    kafkaDf.writeStream
       .format("kafka")
-      .option("kafka.bootstrap.servers", servers.mkString(","))
-      .option("topic", output_identifier)
-      .option("kafka.security.protocol", "SASL_SSL")
-      .option("kafka.sasl.mechanism", "PLAIN")
-      .option("kafka.sasl.jaas.config", s"org.apache.kafka.common.security.plain.PlainLoginModule required username='$apiKey' password='$apiSecret';")
-      .option("kafka.session.timeout.ms", 45000)
-      .option("kafka.client.dns.lookup","use_all_dns_ips")
+      .options(commonKafkaOptions ++ authOptions)
       .option("checkpointLocation", checkpointLocation)
       .option("failOnDataLoss", false)
       .start()
-
   }
 
   override def writeBatch(df: DataFrame): Unit = {
+    val kafkaDf = prepareKafkaDf(df)
 
-    val kafkaDf = idColumnName match {
-      case Some(c) => df.selectExpr(s"$c as key", "to_json(struct(*)) as value")
-      case None => df.selectExpr("to_json(struct(*)) as value")
-    }
-
-    val plain = kafkaDf
-      .write
+    kafkaDf.write
       .format("kafka")
-      .option("kafka.bootstrap.servers", servers.mkString(","))
-      .option("topic", output_identifier)
-      .option("kafka.session.timeout.ms", 45000)
-      .option("kafka.client.dns.lookup", "use_all_dns_ips")
-
-    val withAuth = setWriteAuthentication(plain)
-
-    withAuth.save()
+      .options(commonKafkaOptions ++ authOptions)
+      .save()
   }
-
 }
